c548f280fe41525041038fad5bee03cea251334e
bijuaryal17@gmail.com <bijuaryal17@gmail.com>
2024-09-28 22:28:53 +0545

    Firebase Setup

---
+++ lib/consts/colors.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Mode Colors
+  static const Color lightBackground = Color(0xFFF7F8FC); // Light Grayish
+  static const Color lightPrimary = Color(0xFF4A90E2); // Sky Blue
+  static const Color lightSecondary = Color(0xFF00796B); // Teal
+  static const Color lightText = Color(0xFF333333); // Dark Gray for text
+  static const Color lightMutedText =
+      Color(0xFF757575); // Muted gray for secondary text
+  static const Color lightSendButton =
+      Color(0xFF00BCD4); // Bright Cyan for send button
+  static const Color lightInputField =
+      Color(0xFFFFFFFF); // White for text input background
+  static const Color lightAccent =
+      Color(0xFFFFC107); // Amber for accent elements
+  static const Color lightBorder =
+      Color(0xFFE0E0E0); // Light gray for borders/dividers
+
+  // Dark Mode Colors
+  static const Color darkBackground =
+      Color(0xFF121212); // Very dark gray for background
+  static const Color darkPrimary =
+      Color(0xFFBB86FC); // Light Purple for primary elements
+  static const Color darkSecondary =
+      Color(0xFF03DAC6); // Teal for secondary elements
+  static const Color darkText =
+      Color(0xFFE0E0E0); // Light gray for text on dark backgrounds
+  static const Color darkMutedText =
+      Color(0xFFB0BEC5); // Muted blue-gray for secondary text
+  static const Color darkSendButton =
+      Color(0xFF03DAC6); // Teal for the send button
+  static const Color darkInputField =
+      Color(0xFF1E1E1E); // Dark gray for input field background
+  static const Color darkAccent =
+      Color(0xFFFFC107); // Amber for accent elements in dark mode
+  static const Color darkBorder =
+      Color(0xFF333333); // Dark gray for borders/dividers
+
+  static const Color error = Colors.red;
+}
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  cloud_firestore
+  connectivity_plus
+  emoji_picker_flutter
+  firebase_auth
+  firebase_core
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
